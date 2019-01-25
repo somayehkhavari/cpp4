20af88f767f7fac2d06983ce653a677f0625c1c1
somayeh khavari <46685906+somayehkhavari@users.noreply.github.com>
2019-01-25 15:28:44 +0330

    Add files via upload

---
+++ jadval zarb.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h> 
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+	int a,s;
+	for(a=1;a<=10;a++)
+	{
+		for(s=1;s<=10;s++)
+		cout<<a*s<<'\t';
+		cout<<endl;
+	}
+}
